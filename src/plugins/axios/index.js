efa5eb6b8fce143424ae984d3e9108a0f2674e47
姜天红 <gou00shuang00yue@outlook.com>
2019-05-26 14:32:25 +0800

    处理全局请求返回参数

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
+axios.interceptors.response.use(response => {
+  let { data, status } = response
+  if (status === 200) {
+    return data
+  }
+})
+
 export default {
   install (Vue) {
     Vue.prototype.$axios = axios
