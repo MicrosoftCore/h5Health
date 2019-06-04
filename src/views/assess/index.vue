88adfcab847d285c561c1d90c7fff6f6d6ba0a30
姜天红 <gou00shuang00yue@outlook.com>
2019-06-04 17:22:55 +0800

    评估功能联调

--- src/views/assess/index.vue
+++ src/views/assess/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="assess">
-    <div class="assess-wrapper" v-if="idqtnaire">
+    <div class="assess-wrapper" v-if="id">
       <tab>
         <tab-item selected @on-item-click="onItemClick">近期</tab-item>
         <tab-item @on-item-click="onItemClick">终身</tab-item>
       </tab>
       <div class="flex-column__stretch">
-        <div id="chart1" style="width: 100%; height: 250px;"></div>
+        <div ref="instance" style="width: 100%; height: 250px;"></div>
         <x-button type="primary" mini @click.native="onDetail">查看风险</x-button>
         <div class="assess-wrapper__card">
           <group>
@@ -16,8 +16,12 @@
             </group-title>
             <cell-box>
               <ul>
-                <li>您未来五年的发病风险是{{ cdcqtnaire.fyrsRisk || 0 }},为一般人群的{{ cdcqtnaire.fyrsMultiplerisk || 0 }}倍;</li>
-                <li>您终身发病风险是{{ cdcqtnaire.lifetimeRisk || 0 }}, 为一般人群的{{ cdcqtnaire.lifetimeMultiplerisk || 0 }}倍;</li>
+                <li
+                  v-if="selected === 0"
+                >您未来五年的发病风险是{{ cdcqtnaire.fyrsRisk || 0 }},为一般人群的{{ cdcqtnaire.fyrsMultiplerisk || 0 }}倍;</li>
+                <li
+                  v-if="selected === 1"
+                >您终身发病风险是{{ cdcqtnaire.lifetimeRisk || 0 }}, 为一般人群的{{ cdcqtnaire.lifetimeMultiplerisk || 0 }}倍;</li>
                 <li>如果您调整您的生活习惯, 风险值将会降低{{ cdcqtnaire.reducerisk || 0 }}。</li>
               </ul>
             </cell-box>
@@ -44,7 +48,6 @@
 </template>
 
 <script>
-import echarts from 'echarts'
 import service from '@/common/service'
 import { XButton, Tab, TabItem, Group, GroupTitle, CellBox } from 'vux'
 import { mapGetters } from 'vuex'
@@ -60,220 +63,300 @@ export default {
   },
   data() {
     return {
-      cdcqtnaire: {}
+      selected: 0,
+      cdcthreshold: {},
+      cdcqtnaire: {},
+      fyrsRiskColor: '',
+      fyrsRiskTips: '',
+      lifetimeRiskColor: '',
+      lifetimeRiskTips: ''
     }
   },
   computed: {
-    ...mapGetters('question', ['idqtnaire'])
+    ...mapGetters('question', ['idqtnaire']),
+    id() {
+      let { idqtnaire = '' } = this.$route.query
+      return idqtnaire || this.idqtnaire
+    },
+    riskColor() {
+      return this.selected === 0 ? this.fyrsRiskColor : this.lifetimeRiskColor
+    },
+    riskTips() {
+      return this.selected === 0 ? this.fyrsRiskTips : this.lifetimeRiskTips
+    },
+    riskType() {
+      return this.selected === 0 ? '(5-10年)' : '终身'
+    },
+    riskValue() {
+      let {
+        fyrsLowrisk,
+        fyrsHighrisk,
+        lifetimeLowrisk,
+        lifetimeHighrisk
+      } = this.cdcthreshold
+
+      let { fyrsRisk, lifetimeRisk } = this.cdcqtnaire
+
+      if (this.selected === 0) {
+        return {
+          value1: fyrsRisk,
+          value2: (fyrsLowrisk + fyrsHighrisk) / 2
+        }
+      } else {
+        return {
+          value1: lifetimeRisk,
+          value2: (lifetimeLowrisk + lifetimeHighrisk) / 2
+        }
+      }
+    }
   },
   methods: {
+    onSetOption() {
+      let instance = echarts.init(this.$refs.instance)
+
+      instance.setOption({
+        backgroundColor: '#fff',
+        xAxis: {
+          show: false
+        },
+        yAxis: {
+          show: false
+        },
+        series: [
+          {
+            type: 'gauge',
+            radius: '70%',
+            startAngle: 0,
+            endAngle: 359.9,
+            splitNumber: 100,
+            hoverAnimation: true,
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              length: 25,
+              lineStyle: {
+                width: 3,
+                color: '#fff'
+              }
+            },
+            axisLabel: {
+              show: false
+            },
+            pointer: {
+              show: false
+            },
+            axisLine: {
+              lineStyle: {
+                opacity: 0
+              }
+            },
+            detail: {
+              show: false
+            },
+            data: [
+              {
+                value: 0
+              }
+            ]
+          },
+          {
+            type: 'pie',
+            radius: ['60%', '70%'],
+            silent: true,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            z: 0,
+            zlevel: 0,
+            data: [
+              {
+                value: this.riskValue.value1,
+                label: {
+                  normal: {
+                    show: false
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    color: this.riskColor
+                  }
+                }
+              },
+              {
+                value: this.riskValue.value2,
+                label: {
+                  normal: {
+                    show: false
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    color: '#DDDDDD'
+                  }
+                }
+              }
+            ]
+          },
+          {
+            type: 'pie',
+            radius: ['52%', '53%'],
+            silent: true,
+            z: 0,
+            zlevel: 0,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              }
+            },
+            data: [
+              {
+                value: 0,
+                label: {
+                  normal: {
+                    show: true,
+                    formatter: `{c|${this.riskTips}} {b|${
+                      this.riskType
+                    }} \n {a|综合患癌风险值}`,
+                    rich: {
+                      c: {
+                        color: '#333',
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        lineHeight: 20
+                      },
+                      b: {
+                        color: '#333',
+                        fontSize: 13
+                      },
+                      a: {
+                        align: 'center',
+                        color: '#999',
+                        fontSize: 12,
+                        lineHeight: 20
+                      }
+                    },
+                    position: 'center'
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    color: '#DDDDDD'
+                  }
+                }
+              }
+            ]
+          },
+          {
+            type: 'scatter',
+            symbolSize: 20,
+            itemStyle: {
+              color: '#39BF68'
+            },
+            data: [
+              {
+                value: [0.7, 1],
+                symbolSize: 4
+              },
+              {
+                value: [3.3, 8],
+                symbolSize: 3
+              },
+              {
+                value: [2.3, 5.6],
+                symbolSize: 4
+              },
+              {
+                value: [0.5, 7],
+                symbolSize: 8
+              },
+              {
+                value: [2.8, 0],
+                symbolSize: 8
+              }
+            ]
+          }
+        ]
+      })
+    },
     onDetail() {
       this.$router.push({
         name: 'risk',
         params: {
-          idqtnaire: this.idqtnaire
+          idqtnaire: this.id
         }
       })
     },
-    onItemClick() {}
+    onItemClick(e) {
+      this.selected = e
+      this.onSetOption()
+    }
   },
   async mounted() {
-    let chart1 = echarts.init(document.getElementById('chart1'))
+    let cdcthreshold = await service['cdcthreshold.queryinfo']({
+      params: {
+        idqtnaire: this.id
+      }
+    })
 
-    chart1.setOption({
-      backgroundColor: '#fff',
-      xAxis: {
-        show: false
-      },
-      yAxis: {
-        show: false
-      },
-      series: [
-        {
-          name: '',
-          type: 'gauge',
-          radius: '70%',
-          startAngle: 0,
-          endAngle: 359.9,
-          splitNumber: 100,
-          hoverAnimation: true,
-          axisTick: {
-            show: false
-          },
-          splitLine: {
-            length: 25,
-            lineStyle: {
-              width: 3,
-              color: '#fff'
-            }
-          },
-          axisLabel: {
-            show: false
-          },
-          pointer: {
-            show: false
-          },
-          axisLine: {
-            lineStyle: {
-              opacity: 0
-            }
-          },
-          detail: {
-            show: false
-          },
-          data: [
-            {
-              value: 0,
-              name: ''
-            }
-          ]
-        },
-        {
-          name: '',
-          type: 'pie',
-          radius: ['60%', '70%'],
-          silent: true,
-          label: {
-            normal: {
-              show: false,
-              position: 'center'
-            }
-          },
-          labelLine: {
-            normal: {
-              show: false
-            }
-          },
-          z: 0,
-          zlevel: 0,
-          data: [
-            {
-              value: 60.5,
-              name: '',
-              label: {
-                normal: {
-                  show: false
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: '#39BF68'
-                }
-              }
-            },
-            {
-              value: 90.6,
-              name: '',
-              label: {
-                normal: {
-                  show: false
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: '#DDDDDD'
-                }
-              }
-            }
-          ]
-        },
-        {
-          name: '',
-          type: 'pie',
-          radius: ['52%', '53%'],
-          silent: true,
-          z: 0,
-          zlevel: 0,
-          label: {
-            normal: {
-              show: false,
-              position: 'center'
-            }
-          },
-          data: [
-            {
-              value: 0,
-              name: '',
-              label: {
-                normal: {
-                  show: true,
-                  formatter: '{c|低危} {b|(5-10年)} \n {a|综合患癌风险值}',
-                  rich: {
-                    c: {
-                      color: '#333',
-                      fontSize: 20,
-                      fontWeight: 'bold',
-                      lineHeight: 20
-                    },
-                    b: {
-                      color: '#333',
-                      fontSize: 13
-                    },
-                    a: {
-                      align: 'center',
-                      color: '#999',
-                      fontSize: 12,
-                      lineHeight: 20
-                    }
-                  },
-                  position: 'center'
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: '#DDDDDD'
-                }
-              }
-            }
-          ]
-        },
-        {
-          type: 'scatter',
-          symbolSize: 20,
-          itemStyle: {
-            color: '#39BF68'
-          },
-          data: [
-            {
-              value: [0.7, 1],
-              symbolSize: 4
-            },
-            {
-              value: [3.3, 8],
-              symbolSize: 3
-            },
-            {
-              value: [2.3, 5.6],
-              symbolSize: 4
-            },
-            {
-              value: [0.5, 7],
-              symbolSize: 8
-            },
-            {
-              value: [2.8, 0],
-              symbolSize: 8
-            }
-          ]
-        }
-      ]
-    })
-    let result = await service['cdcthreshold.queryinfo']({
+    let cdcqtnaire = await service['cdcqtnaire.queryinfo']({
       params: {
-        idqtnaire: this.idqtnaire
+        idqtnaire: this.id
       }
     })
-    this.cdcqtnaire = await service['cdcqtnaire.queryinfo']({
-      params: {
-        idqtnaire: this.idqtnaire
-      }
-    })
-    let result2 = await service['description.queryinfos']({
-      params: {
-        idqtnaire: this.idqtnaire
-      }
-    })
-    console.log(result, this.cdcqtnaire, result2)
+
+    let {
+      fyrsLowrisk,
+      fyrsHighrisk,
+      lifetimeLowrisk,
+      lifetimeHighrisk
+    } = cdcthreshold
+
+    let { fyrsRisk, lifetimeRisk } = cdcqtnaire
+
+    this.cdcthreshold = cdcthreshold
+    this.cdcqtnaire = cdcqtnaire
+
+    if (fyrsRisk < fyrsLowrisk) {
+      this.fyrsRiskColor = '#39BF68'
+      this.fyrsRiskTips = '低危'
+    }
+
+    if (fyrsRisk > fyrsLowrisk && fyrsRisk < fyrsHighrisk) {
+      this.fyrsRiskColor = '#FFBE00'
+      this.fyrsRiskTips = '中危'
+    }
+
+    if (fyrsRisk > fyrsHighrisk) {
+      this.fyrsRiskColor = '#F43530'
+      this.fyrsRiskTips = '高危'
+    }
+
+    if (lifetimeRisk < lifetimeLowrisk) {
+      this.lifetimeRiskColor = '#39BF68'
+      this.lifetimeRiskTips = '低危'
+    }
+
+    if (lifetimeRisk > lifetimeLowrisk && lifetimeRisk < lifetimeHighrisk) {
+      this.lifetimeRiskColor = '#FFBE00'
+      this.lifetimeRiskTips = '中危'
+    }
+
+    if (lifetimeRisk > lifetimeHighrisk) {
+      this.lifetimeRiskColor = '#F43530'
+      this.lifetimeRiskTips = '高危'
+    }
+
+    this.onSetOption()
   }
 }
 </script>
